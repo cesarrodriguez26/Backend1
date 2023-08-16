942a4df79dea6a3d2c14b57c35005bf330bd7966
cesarrodriguez26 <124197857+cesarrodriguez26@users.noreply.github.com>
2023-08-16 14:51:52 +0200

    Servidor Express

--- src/server.js
+++ src/server.js
@@ -8,7 +8,19 @@ const productos = new ProductManager();
 const readProducts = productos.readProducts();
 
 app.get("/products", async(req,res) => {
-    res.send(await readProducts);
+    let limit = parseInt(req.query.limit);
+if(!limit) return res.send(await readProducts) 
+
+    let allProducts= await readProducts
+    let productLimit = allProducts.slice(0, limit)
+    res.send(productLimit);
+});
+
+app.get("/products/:id", async(req,res) => {
+    let id = parseInt(req.params.id);
+    let allProducts= await readProducts;
+    let productById = allProducts.find(product => product.id === id)
+    res.send(productById)
 });
 
 const PORT = 8080;
